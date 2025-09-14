60243fe05c808648f45c2c8c65cebb67f8ab79af
Sabareeswaran S <sabarishanmugam357@gmail.com>
2025-09-14 23:41:49 +0530

    Created

---
+++ FactorialNum.java
@@ -0,0 +1,10 @@
+public class FactorialNum{
+    public static void main(String[] args){
+        int num=5;
+        long fact=1;
+        for (int i=1;i<=num;i++){
+            fact=fact*i;
+        }
+        System.out.print(num);
+    }
+}
