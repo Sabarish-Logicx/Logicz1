44cd4e7331819280b44755ddfddbe3ec0578ff23
Sabareeswaran S <sabarishanmugam357@gmail.com>
2025-11-10 23:54:13 +0530

    Modified

--- log1.java
+++ log1.java
@@ -3,10 +3,12 @@ public class log1{
 public static void main(String []args){
     int n=12;
     int m=8;
+
+
     Scanner sc=new Scanner(System.in);
     int n=sc.nextInt();
     int m=sc.nextInt();
-    System.out.println(n+m);
+    System.out.println(n+m);    //20
 
 }
 }
