62586b0e0de4ef30b5e096d70d7cf519cfde3733
Sabareeswaran S <sabarishanmugam357@gmail.com>
2025-11-10 23:53:38 +0530

    Modified

--- log1.java
+++ log1.java
@@ -1,6 +1,8 @@
 import java.util.Scanner;
 public class log1{
 public static void main(String []args){
+    int n=12;
+    int m=8;
     Scanner sc=new Scanner(System.in);
     int n=sc.nextInt();
     int m=sc.nextInt();
