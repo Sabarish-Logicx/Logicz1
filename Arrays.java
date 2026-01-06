a2eac1fc2d402da6036b8ad716b751cb30f2f291
Sabareeswaran S <sabarishanmugam357@gmail.com>
2026-01-06 22:55:42 +0530

    Created Arrays.java

---
+++ Arrays.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+class Arrays{
+    public static void main(String []args){
+        int[] arr={1,3,2,4};
+        int n=5;
+        Arrays.sort(arr);
+        for(int i=0;i<a.length;i++){
+            if(a[i]!=i+1){
+                missing=i+1;
+                break;
+            }
+        }
+        System.out.println("Missing No :"+Missing);
+    }
+}
