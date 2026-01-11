e395b5fca70afd4fb2151001533aa747d8c0993f
Sabareeswaran S <sabarishanmugam357@gmail.com>
2026-01-11 15:48:02 +0530

    Updated Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,9 +1,10 @@
+//Missing Number in an Array
 import java.util.Arrays;
 class Arrays{
     public static void main(String []args){
-        int[] arr={1,3,2,4};
+        int[] arr={1,3,2,5};
         int n=5;
-        Arrays.sort(arr);
+        Arrays.sort(arr);                 //Sorting {1,2,3,5}
         for(int i=0;i<a.length;i++){
             if(a[i]!=i+1){
                 missing=i+1;
