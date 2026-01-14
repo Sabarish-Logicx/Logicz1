f2da52c253906e587e32ccf390a56c06f1293625
Sabareeswaran S <sabarishanmugam357@gmail.com>
2026-01-15 00:14:54 +0530

    Created ArmNum.java

---
+++ ArmNum.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class ArmNum{
+    public static void main (String [] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Number :");
+        int num=sc.nextInt();
+
+        int original=num;
+        //int sum=0;
+
+        //count digit
+        int digits=String.valueOf(num).length();
+        int sum=0;
+
+        while(num>0){
+            int digit=num%10;
+            sum+=Math.pow(digit,digits);
+            num/=10;
+        }
+
+        if(sum==original){
+            System.out.println("Armstrong Number");
+        }else{
+            System.out.println("Not Armstrong Number");
+        }
+
+
+        }
+
+}
