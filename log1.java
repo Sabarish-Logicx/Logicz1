a76bc9ca6ca9d96201025906e3f8025d51e8e0bb
Sabareeswaran S <sabarishanmugam357@gmail.com>
2026-01-21 23:52:36 +0530

    updated

--- log1.java
+++ log1.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 public class log1{
 public static void main(String []args){
-    int n=12;
-    int m=8;
+   // int n=12;
+    //int m=18;
 
-
-    Scanner sc=new Scanner(System.in);
+   Scanner sc=new Scanner(System.in);
+   System.out.println("Enter a first Number :"); 
     int n=sc.nextInt();
     int m=sc.nextInt();
-    System.out.println(n+m);    //20
+    
+    int sum =n+m;
+    
+    System.out.println(sum);    //20
 
 }
 }
