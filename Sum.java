4554950d50dbdb1876d080f8cea4e16d8688a459
Sabareeswaran S <sabarishanmugam357@gmail.com>
2026-01-21 23:55:59 +0530

    updated

--- Sum.java
+++ Sum.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class log1{
+public class Sum{
 public static void main(String []args){
    // int n=12;
     //int m=18;
@@ -7,11 +7,12 @@ public static void main(String []args){
    Scanner sc=new Scanner(System.in);
    System.out.println("Enter a first Number :"); 
     int n=sc.nextInt();
+    System.out.println("Enter a second Number :");
     int m=sc.nextInt();
     
     int sum =n+m;
     
-    System.out.println(sum);    //20
+    System.out.println("Answer is :"+sum);    //20
 
 }
 }
