01456b008961146717f9d9102593ababb94bb831
Sabareeswaran S <sabarishanmugam357@gmail.com>
2026-01-26 00:00:14 +0530

    updated

---
+++ AbudantNum.java
@@ -0,0 +1,10 @@
+class AbudantNum{
+    public statc void main(String[] args){
+        int num=12;
+        int sum=0;
+
+        for(int i=0;i<=num;i++){
+            if(num%i==0){}
+        }
+    }
+}
