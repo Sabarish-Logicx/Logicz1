e5e87e5fd83f6268a3446cd9162afecf5029a9ec
Sabareeswaran S <sabarishanmugam357@gmail.com>
2026-02-04 23:32:34 +0530

    Created AddFrac.java

---
+++ AddFrac.java
@@ -0,0 +1,13 @@
+
+class AddFrac{
+    public static void main (String[] args){
+        int num1=1,den1=2;
+        int num2=3,den2=4;
+
+        int numerator=num1*den2+num2*den1;
+        int denominator=den1*den2;
+
+        System.out.println("sum = "+numerator+"/"+denominator);
+    }
+    }
+
