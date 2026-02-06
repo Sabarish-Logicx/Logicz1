63aea1614352b45d78840e015d301171f31acfc3
Sabareeswaran S <sabarishanmugam357@gmail.com>
2026-02-06 23:14:44 +0530

    Created FactOfNum.java

---
+++ FactOfNum.java
@@ -0,0 +1,10 @@
+class FactOfNum{
+    public static void main(String args[]){
+        int num=6;
+
+        System.out.println("Factorial Of "+num+" are:");
+        for(int i=1;i<=num;i++){
+            System.out.println(i);
+        }
+    }
+}
