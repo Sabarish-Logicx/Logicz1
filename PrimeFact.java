8bed43dc5abc250f450a4144480691568fb3f6ad
Sabareeswaran S <sabarishanmugam357@gmail.com>
2026-02-09 22:34:12 +0530

    Modified PrimeFact.java

--- PrimeFact.java
+++ PrimeFact.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
 class PrimeFact{
     public static void main(String [] args)
 {
-    int a=55;
-    System.out.println("Prime factor of"+n+"are :");
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter a Number : ");
+    int n=sc.nextInt();
+    
+    //int n=36;
+    
+    System.out.print("Prime factor of "+n+" are : ");  // 2 2 3 3
 
     while(n%2==0){
         System.out.print(2+" ");
+        n=n/2;
     }
 
     for(int i=3;i*i<=n;i+=2){
