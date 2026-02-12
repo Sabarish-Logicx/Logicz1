762aee6dd9032a74c7b3459030eefb915646ae32
Sabareeswaran S <sabarishanmugam357@gmail.com>
2026-02-12 22:39:31 +0530

    Modified PrimeFactOfNum.java

--- FactOfNum.java
+++ FactOfNum.java
@@ -1,12 +1,22 @@
+import java.util.Scanner;
 class FactOfNum{
     public static void main(String args[]){
-        int num=6;
+        //int num=6;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a Num : ");
+        int num=sc.nextInt();
 
-        System.out.println("Factorial Of "+num+" are:");
+        int sum=0;
+       
+
+       // System.out.println("Factorial Of "+num+" are : ");
         for(int i=1;i<=num;i++){
             if(num %i==0){
-            System.out.println(i);               // 1 2 3 6
+            System.out.println(i);
+            sum =sum+i;               // 1 2 3 6
             }                    
         }
+    System.out.println("Addition of Fact are : "+sum);
     }
+
 }
