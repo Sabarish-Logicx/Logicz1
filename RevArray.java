b71cb627e3f7b9b69e22d2c98fa833b9fc13ad17
Sabareeswaran S <sabarishanmugam357@gmail.com>
2026-03-06 23:59:51 +0530

    Modifieded RevArray.java

--- RevArray.java
+++ RevArray.java
@@ -2,6 +2,10 @@ class RevArray{
 public static void main(String[] args){
     int[] arr={1,2,3,4,5,6};
 
+    for(int i=arr.length-1;i>=0;i--){
+        System.out.print(arr[i]);
+    }
+
     int start=0;
     int end =arr.length-1;
 
