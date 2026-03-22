b8ecd97ab65807b6d23f0d5836ebe19e2c00eb59
Sabareeswaran S <sabarishanmugam357@gmail.com>
2026-03-22 23:47:33 +0530

    Modifieded

--- RevArray.java
+++ RevArray.java
@@ -3,11 +3,11 @@ public static void main(String[] args){
     int[] arr={1,2,3,4,5,6};
 
     for(int i=arr.length-1;i>=0;i--){
-        System.out.print(arr[i]);         //654321
+        System.out.print(arr[i]+" ");         //654321
     }
 
 
-    int start=0;
+    /*int start=0;
     int end =arr.length-1;
 
     while(start<end){
@@ -20,6 +20,6 @@ public static void main(String[] args){
     }
     for(int num:arr){
         System.out.print(num+" ");         //6 5 4 3 2 1
-    }
+    }*/
 }
 }
