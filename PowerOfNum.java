c5e5d8408f505d822bb22b6be5c4aa7b653a5398
Sabareeswaran S <sabarishanmugam357@gmail.com>
2026-04-01 23:44:08 +0530

    Modified PowerOfNum.java

--- PowerOfNum.java
+++ PowerOfNum.java
@@ -1,11 +1,18 @@
 public class PowerOfNum{
     public static void main(String [] args){
+
+        //In build method
+        double Result=Math.pow(2,5);
+
+        //Using loop
         int base=2;
         int exp=5;
         int res=1;
         for(int i=1;i<=exp;i++){
             res=res*base;
+
         }
+
         System.out.println("Result is : "+res);            //2^5=32
     }
 }
