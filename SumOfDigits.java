0786f1805b4b992dadf81b62a27f379ef8e37718
Sabareeswaran S <sabarishanmugam357@gmail.com>
2026-04-24 23:17:24 +0530

    Modifieded SumOfDigits.java

---
+++ SumOfDigits.java
@@ -0,0 +1,14 @@
+public class SumOfDigits{
+    public static void main(String[] args){
+        int num=22;
+
+        int sum=0;
+
+        while(num!=0){
+            int digit=num%10;
+            sum=sum+digit;
+            num=num/10;
+            }
+            System.out.println("Sum of Digits="+sum);
+    }
+}
